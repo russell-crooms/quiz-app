69a4bbcf0dfedabb93d59331e6e4a4d9c06d973d
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-29 21:55:57 -0600

    Added questions to our javascript file

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions =  [
+    {
+        question: "Which animal is the largest in the world?", 
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    }, 
+    {
+        question: "Which is the smallest country in the world?", 
+        answers: [
+            { text: "Vatican City", correct: true },
+            { text: "Bhutan", correct: false },
+            { text: "Nepal", correct: false },
+            { text: "Sri Lanka", correct: false },
+        ]
+    },
+    {
+        question: "Which is the largest desert in the world?", 
+        answers: [
+            { text: "Kalahari", correct: false },
+            { text: "Gobi", correct: true },
+            { text: "Sahara", correct: false },
+            { text: "Antartica", correct: true },
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?", 
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "North America", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    },
+
+];
