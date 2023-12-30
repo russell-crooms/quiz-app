5d5fd161e010eecd7abdfbd91265f03540ce1e44
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-29 22:07:57 -0600

    Added logic to display the questions in javascript

--- script.js
+++ script.js
@@ -37,3 +37,23 @@ const questions =  [
     },
 
 ];
+
+const questionElement = document.getElementById("question"); 
+const answerButton = document.getElementById("answer-buttons"); 
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0; 
+
+function startQuiz() {
+    currentQuestionIndex = 0; 
+    score = 0; 
+    nextButton.innerHTML = "NEXT"; 
+    showQuestion(); 
+}
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex]; 
+    let questionNo = currentQuestionIndex + 1; 
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
