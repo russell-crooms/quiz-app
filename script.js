af16fb9f010643dc67ce67d3f6fa39f4b212f8b9
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-29 22:20:38 -0600

    Removed additional answers in javascript

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const questions =  [
 ];
 
 const questionElement = document.getElementById("question"); 
-const answerButton = document.getElementById("answer-buttons"); 
+const answerButtons = document.getElementById("answer-buttons"); 
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -53,7 +53,25 @@ function startQuiz() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex]; 
     let questionNo = currentQuestionIndex + 1; 
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button"); 
+        button.innerHTML = answer.text;
+        button.classList.add("btn"); 
+        answerButtons.appendChild(button); 
+    });
 }
+
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+      answerButtons.removeChild(answerButtons.firstChild);  
+    }
+}
+
+startQuiz(); 
